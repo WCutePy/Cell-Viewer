bc78918bf71c71822b2461238a5cf7d1eeff006c
wouter <post4wouter.t@gmail.com>
2024-12-16 12:28:13 +0100

    Added an annotation viewer and the ability to change the annotation It is not possible to see annotations from the job. It is not possible to delete annotations from the annotations themselves.
    If an annotation is unused, and you set it to be unused, it will not get automatically deleted yet

--- apps/cellviewer/components/label_matrix_input_fields.py
+++ apps/cellviewer/components/label_matrix_input_fields.py
@@ -8,7 +8,9 @@ class LabelMatrixInputFieldsComponent(Component):
             "matrix_name": matrix_name,
             "row_names": row_names,
             "col_names": col_names,
-            "cell_names": cell_names
+            "cell_names": cell_names,
+            "default_rows": ",,,".join(row_names),
+            "default_cols": ",,,".join(col_names),
         }
     
 
@@ -26,18 +28,21 @@ class LabelMatrixInputFieldsComponent(Component):
         style="grid-template-columns: repeat({{ col_names|length|add:1 }}, minmax(0, 1fr))">
         <div></div>
         {% for col in col_names %}
-             <input type="text" name="col" id="col-{{ col }}" maxlength="255" placeholder="{{ col }}"
+             <input type="text" name="col" id="col-{{ col }}" maxlength="255" value="{{ col }}"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
         {% endfor %}
     
         {% for row in row_names %}
-            <input type="text" name="row" id="row-{{ row }}" maxlength="255" placeholder="{{ row }}"
+            <input type="text" name="row" id="row-{{ row }}" maxlength="255" value="{{ row }}"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
             {% for cell in cell_names|at_index_i:forloop.counter0 %}
-                <input type="text" name="cell" id="cell-{{ cell }}" maxlength="255" placeholder="{{ cell }}"
+                <input type="text" name="cell" id="cell-{{ cell }}" maxlength="255" value="{{ cell }}"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
             {% endfor %}
             
         {% endfor %}
     </div>
+    
+    <input type="hidden" name="default-rows" value="{{ default_rows }}">
+    <input type="hidden" name="default-cols" value="{{ default_cols }}">
     """
---
+++ apps/cellviewer/views/annotations.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from apps.cellviewer.models.LabelMatrix import LabelMatrix
+from apps.cellviewer.views.index import load_labels_from_request
+
+
+def saved_annotations_page(request):
+    annotations = LabelMatrix.objects.get_all_of_user(request.user)
+
+    fields = ["id", "matrix_name", "dimension_string", "public",
+               "keep_when_unused"]
+    headers = ["name", "dimension", "publicly available", "will remain if unused"]
+    annotations = list(annotations.values_list(*fields))
+    
+    context = {
+        "header": headers,
+        'jobs': annotations,
+        'segment': 'annotations',
+    }
+    return render(request, "cellviews/saved_annotations.html", context)
+
+
+def annotation_page(request, annotation_id: int):
+    annotation = LabelMatrix.objects.get(id=annotation_id)
+
+    if annotation.created_by.id != request.user.id:
+        return
+    
+    rows, cols, cells = annotation.get_labels_with_2d_cells
+    
+    context = {
+        "annotation_name": annotation.matrix_name,
+        "row_names": rows,
+        "col_names": cols,
+        "cell_names": cells,
+        "keep_when_unused": annotation.keep_when_unused,
+        "public": annotation.public,
+    }
+
+    return render(request, "cellviews/display_saved_annotation.html", context)
+
+
+def edit_annotation(request, annotation_id: int):
+    annotation = LabelMatrix.objects.get(id=annotation_id)
+
+    if annotation.created_by.id != request.user.id:
+        return
+    
+    name = request.POST.get("name")
+    _, labels = load_labels_from_request(request)
+    
+    keep_when_unused = request.POST.get('keep_when_unused') is not None
+    public = request.POST.get('public') is not None
+    
+    annotation.update(*labels, name, keep_when_unused, public)
+    
+    return redirect("cellviewer:annotation_page", annotation_id)
