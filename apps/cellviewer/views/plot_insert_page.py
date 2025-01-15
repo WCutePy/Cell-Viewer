70919f1dd6d2b7f61fc9b2aa9e928dba74999b6f
wouter <post4wouter.t@gmail.com>
2025-01-15 03:20:50 +0100

    Added the ability to download a csv of the heatmaps.
    This can be added and implemented on other pages as well, through the addition of a javascript function to download files.

--- apps/cellviewer/views/plot_insert_page.py
+++ apps/cellviewer/views/plot_insert_page.py
@@ -11,35 +11,57 @@ from apps.cellviewer.util.index_helpers import load_and_save_processing
 
 
 def plot_insert_element(df: pl.dataframe, labels,
-                        pre_filter_substance_threshold=None,
-                        substance_threshold=None,
-                        include=["all"]
+                        pre_filter_substance_threshold: list[float] = None,
+                        substance_threshold: list[float] = None,
+                        include: list[str]=("all",),
+                        name=None
                         ):
     """
     Does not directly render a view.
-    Creates the necessary context to use the include dash_embed (to be changed).
+    Creates the necessary context to use the include of the
+    visualization page base_visualization.html
     
-    Through the use of being saved to the database,
-    but also by the data not being saved and sent by the user every
-    time. The latter is more inefficient than the prior.
-    This page does not handle this part and simply generates
-    the plot section.
-    This makes it possible for the surrounding infrastructure to
-    change.
+    This function expects all the information
+    that could be necessary to render page to be preprocessed
+    in their expected formats.
     
-    Another function to generate an updated double positive
-    heatmap will be below. This too requires the pre
-    handling of variables.
+    It is possible to get the full context, or only get a part of
+    the context. This is done because some information,
+    such as the filtered heatmaps, is expected to be reloaded
+    multiple times with different values. As this is the case,
+    it would not be smart or recommended to also recreate the histograms
+    every time when they remain unused.
+    The variable include is used for this, however the standar
+    is rather unclear.
+    Currently, it is set as a tuple of seperate elements,
+    so it might be possible in the future to frankestein multiple
+    parts together.
+    However currently this is irrelevant, as there are two options.
     
+    It is able to do two steps of thresholds.
+    A pre load threshold, this is data which is not included,
+    and should not be included in the calculations. This is relevant
+    for when stored to the database.
+    However it is not sure if this is expected behaviour, so this
+    should be checked more thoroughly.
+    
+    A filter threshold, this is the filter to apply to calculate the
+    double positives.
     
     Args:
-        df:
-
+        df: a polars dataframe
+        labels: The labels in the list format [row, col, cells]
+        pre_filter_substance_threshold: a list with floats
+        substance_threshold: a list with floats
+        include: a list of strings for the elements.
+            Leaving this empty gives all.
+        name:
+    
     Returns:
+        context dictionary to be used in combination with the
+        base_visualiation.html
 
     """
-    
-    "histogram_data =  [substance_name, histogram, max_value]"
     
     if pre_filter_substance_threshold is None:
         pre_filter_substance_threshold = (0, 0)
@@ -47,10 +69,10 @@ def plot_insert_element(df: pl.dataframe, labels,
     original_df = df
     df = filtered_polars_dataframe(df, pre_filter_substance_threshold)
     
-    if substance_threshold is None:
-        substance_threshold = [0, 0]
-    
     substances = df.columns[3:]
+    
+    if substance_threshold is None:
+        substance_threshold = (0,) * len(substances)
     
     context = {
     }
@@ -80,6 +102,7 @@ def plot_insert_element(df: pl.dataframe, labels,
             "heatmap_total_cell_counts": generate_heatmap_with_label(
                 labels, well_count_matrix, "Count"
             ).to_html(),
+            "file_count_matrix": well_count_matrix.to_csv(),
         })
     
     context.update({
@@ -87,13 +110,19 @@ def plot_insert_element(df: pl.dataframe, labels,
         "sub_and_threshold_str": " and ".join(
             f"{a} for {b}" for a, b in zip(substance_threshold, substances)
         ),
+        
         "heatmap_filtered_cell_counts": generate_heatmap_with_label(
             labels, filtered_well_count_matrix, "Count"
         ).to_html(),
+        "file_filtered_counts": filtered_well_count_matrix.to_csv(),
+        
         "heatmap_percentage": generate_heatmap_with_label(
             labels, well_count_matrix_percent, "Count"
         ).to_html(),
+        "file_double_positives": well_count_matrix_percent.to_csv(),
+        
         "job_id": "-1",
+        "name": name,
     })
     
     return context
@@ -101,16 +130,48 @@ def plot_insert_element(df: pl.dataframe, labels,
 
 def update_filtered_plots(request):
     """
-    Handles the updating of the filtered plots.
-    This is currently done through python.
-    For better performance, and to potentially have the
-    code be less "weird", it could be better
-    to perform this operation through JavaScript.
-    Doing it through JavaScript is preferred, however
-    to not add new systems which might be unknown, it is
-    currently performed similar to how Dash does it.
+    Renders the page element of the filtered plots.
+    Loads base_visualization_filtered_part.html
+    Sending in the context only the rquired information.
     
-    The callback however did need to be manually written.
+    It is possible to call from both a not saved,
+    and a saved file.
+    If it has been saved the job id will be a non -1
+    integer, if it has not been saved it is -1.
+    
+    When it has not been saved, it will load the information
+    relevant through the same function the index load
+    function uses. As such, thanks to the reuse, this function
+    has been abstracted to utils.
+    
+    If it has been saved, it uses the same methodology that
+    saved_jobs uses to load the data. Currently, this is
+    not a function yet, however this too should be abstracted
+    for consistency. Currently, there is code duplication
+    
+    After having the info loaded,
+    It only renders the plots that are relevant and created the
+    filtered part of the page.
+    
+    This is done this way to not require reloading the full page
+    everytime.
+    It does reload a part, instead of updating the plots on
+    the client side, and that is okay. This is as, to more performant
+    than using Dash is, and more performance is currently not required.
+    
+    For better performance, and for more customizability,
+    all visualization, or atleast the visualizations that require
+    a great deal of interaction or customizability should be rendered
+    or updated through javascript.
+    The current interaction functions the same dash does
+    their callbacks. So it's not particularly Django behaviour.
+    I didn't do it with JavaScript, as it would add more to the
+    application in learning curve, and it would take me more time
+    to rewrite from Dash to JavaScript than putting this
+    together.
+    
+    The updated section is loaded through HTMX. A simple way
+    to update a section of a page without JavaScript.
     
     Args:
         request:
@@ -124,6 +185,9 @@ def update_filtered_plots(request):
         
         df = pl.read_csv(files[0])
         
+        if not name:
+            name = "unnamed"
+    
     else:
         # duplicates part with saved_jobs, find a better way
         # to do this.
@@ -143,11 +207,14 @@ def update_filtered_plots(request):
         df = filtered_file.load_polars_dataframe()
         
         pre_filter_substance_threshold = filtered_file.get_substance_thresholds_as_list
+        
+        name = job.name
     
-    substance_threshold = [float(i) for i in request.POST.getlist("substance_threshold")]
+    substance_threshold = [float(i) for i in
+                           request.POST.getlist("substance_threshold")]
     
     context = plot_insert_element(df, labels, pre_filter_substance_threshold,
-                                  substance_threshold)
-    return render(request, "cellviews/visualization/base_visualization_filtered_part.html", context)
-    
-    
+                                  substance_threshold, name=name)
+    return render(request,
+                  "cellviews/visualization/base_visualization_filtered_part.html",
+                  context)
